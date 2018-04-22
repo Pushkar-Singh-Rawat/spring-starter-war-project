72da5a7cc4137fcded716c4925e52629a42833b8
desiCoda <j.pushkarsingh@gmail.com>
2018-04-22 22:33:35 +0530

    updated Spring_main entry point class

--- src/main/java/demo/Spring_main.java
+++ src/main/java/demo/Spring_main.java
@@ -2,11 +2,22 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Spring_main {
+public class Spring_main extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// TODO Auto-generated method stub
+		
+		return application.sources(Spring_main.class);
+	}
+    
     public static void main(String[] args) {
         SpringApplication.run(Spring_main.class, args);
     }
+
+	
 }
